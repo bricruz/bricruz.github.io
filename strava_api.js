a4903b1b456119462858820cb12a589f4d320399
bricruz <55154210+bricruz@users.noreply.github.com>
2022-03-22 18:33:07 -0500

    Update strava_api.js

--- strava_api.js
+++ strava_api.js
@@ -1,31 +1,32 @@
 
 const auth_link = "https://www.strava.com/oauth/token"
 
-function getActivites(res){
+function getActivites(){
 
-    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+    const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=2204c89ead37f6368c7d69aa8da30ba976f4aaf2`
     fetch(activities_link)
         .then((res) => console.log(res.json()))
 }
+getActivites()
 
-function reAuthorize(){
-    fetch(auth_link,{
-        method: 'post',
-        headers: {
-            'Accept': 'application/json, text/plain, */*',
-            'Content-Type': 'application/json'
+// function reAuthorize(){
+//     fetch(auth_link,{
+//         method: 'post',
+//         headers: {
+//             'Accept': 'application/json, text/plain, */*',
+//             'Content-Type': 'application/json'
 
-        },
+//         },
 
-        body: JSON.stringify({
+//         body: JSON.stringify({
 
-            client_id: '79984',
-            client_secret: 'cd8eacd26bec38d4729324ded24080d25d0a6cf5',
-            refresh_token: '701ef1db968b503b19f959fd1e5e9957e0ba9fc6',
-            grant_type: 'refresh_token'
-        })
-    }).then(res => res.json())
-        .then(res => getActivites(res))  
-}
+//             client_id: '79984',
+//             client_secret: 'cd8eacd26bec38d4729324ded24080d25d0a6cf5',
+//             refresh_token: '701ef1db968b503b19f959fd1e5e9957e0ba9fc6',
+//             grant_type: 'refresh_token'
+//         })
+//     }).then(res => res.json())
+//         .then(res => getActivites(res))  
+// }
 
-reAuthorize()
+// reAuthorize()
